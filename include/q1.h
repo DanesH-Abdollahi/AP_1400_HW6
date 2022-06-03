aea5938cb4838f48b904fae187aca4e06548cc2b
DanesH-Abdollahi <danesh.abdollahii@gmail.com>
2022-06-03 17:45:51 +0430

    q1 is Eddited

--- include/q1.h
+++ include/q1.h
@@ -1,6 +1,7 @@
 #ifndef Q1_H
 #define Q1_H
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <map>
 
@@ -8,24 +9,33 @@ namespace q1 {
 template <typename T, typename Func>
 const T gradient_descent(const T& initial_value, const T& step_size, Func function = Func {})
 {
-    T x { initial_value };
-    T y { function(x) };
+    T X { initial_value };
+    T delta { step_size * 0.000001 };
+    T dY_dx {};
+    T X_New {};
 
     while (true) {
-        std::map<T, T> Temp {};
-        Temp[x - step_size] = function(x - step_size);
-        Temp[x] = function(x);
-        Temp[x + step_size] = function(x + step_size);
+        T dY_dX = (function(X + delta) - function(X)) / delta;
+        T X_New = X - step_size * dY_dX;
 
-        const auto Local_min { *min_element(Temp.begin(), Temp.end(),
-            [](const auto a, const auto b) { return a.second < b.second; }) };
-        if (Local_min.first != x) {
-            x = Local_min.first;
-            y = Local_min.second;
-        } else
+        if (std::abs(X_New - X) >= delta)
+            X = X_New;
+        else
             break;
+        // std::map<T, T> Temp {};
+        // Temp[X - step_size] = function(X - step_size);
+        // Temp[X] = function(X);
+        // Temp[X + step_size] = function(X + step_size);
+
+        // const auto Local_min { *min_element(Temp.begin(), Temp.end(),
+        //     [](const auto a, const auto b) { return a.second < b.second; }) };
+        // if (Local_min.first != X) {
+        //     X = Local_min.first;
+        //     Y = Local_min.second;
+        // } else
+        //     break;
     }
-    return x;
+    return X;
 }
 }
 #endif // Q1_H
--- include/q2.h
+++ include/q2.h
@@ -1,38 +1,33 @@
 #ifndef Q2_H
 #define Q2_H
-
 #include <algorithm>
-#include <fstream>
-#include <iostream>
-#include <regex>
-#include <sstream>
+// #include <fstream>
+// #include <iostream>
+// #include <regex>
+// #include <sstream>
 #include <string>
+#include <vector>
 
-// Name, Surname, Age, Smokes, AreaQ, Alkhol
+// // Name, Surname, Age, Smokes, AreaQ, Alkhol
 
-namespace q2 {
+// namespace q2 {
 
-struct Patient {
-    std::string name;
-    size_t age;
-    size_t smokes;
-    size_t area_q;
-    size_t alkhol;
-};
+// // struct Patient {
+// //     std::string name;
+// //     size_t age;
+// //     size_t smokes;
+// //     size_t area_q;
+// //     size_t alkhol;
+// // };
 
-std::vector<Patient> read_file(std::string filename)
-{
+// std::vector<int> read_file(std::string filename) {
 
-    auto ss = std::ostringstream {};
-    std::ifstream input_file(filename);
-    if (!input_file.is_open()) {
-        std::cerr << "Could not open the file - '"
-                  << filename << "'" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-    ss << input_file.rdbuf();
-    // return ss.str();
-}
-}
+//     // auto ss = std::ostringstream {};
+//     // std::ifstream input_file(filename);
+//     // ss << input_file.rdbuf();
+//     // // return ss.str();
+// };
+
+// }
 
 #endif // Q2_H
